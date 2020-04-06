c80d651f77403f37b2ce6341fb5c7bcbe1d0a8d8
guigagb <trindade.guilherme@outlook.com>
2020-04-06 12:07:47 -0300

    subMenu implementado

--- examples/example.js
+++ examples/example.js
@@ -3,35 +3,75 @@ import xMenu from "../xMenu2.js";
 var menu1 = new xMenu.create({
     id: '#meuMenu1',
     buttonLeft: false,
-    disable: true,
-    onCreate: ()=>{
-        console.log('abcde');
+    disable: false,
+    onCreate: () => {
+        // console.log('abcde');
     },
-    onOpen: ()=>{
-        console.log('456');
+    onOpen: (/*btn*/) => {
     },
     itens: {
         oi: {
-            icon: 'fa fa-search',
-            click: function () {  
-                console.log('oi');
+            // icon: 'fa fa-search',
+            // disable: true,
+            checkbox: true,
+            shortkey: {
+                'ctrl1': 'ctrl+shift'
+            },
+            click: function () {
+                console.log('menu1');
             }
         },
-        teste2: {
+        hojee:{
+
+        },
+        testeeeeeee2: {
+            html: 'teste teste teste aaa',
+            icon: 'fa fa-trash',
+            shortkey: {
+                'enter': '13'
+            },
+            click: ()=>{
+                console.log('toy toy');
+            }
+        },
+        lala:{
+            // icon: 'fa fa-link',
+            checkbox: true,
+            shortkey: {
+                'abc': '12'
+            },
+            click: ()=>{
+                console.log('menu2');
+            },
+            subMenu: {
+                onOpen: ()=>{
+                    // console.log('abriuuuuuuuu',a,b.offsetTop, b.offsetLeft);
+                },
+                itens: {
+                    a: {
+                        html: 'teste'
+                    },
+                    b:{
+                        html: 'hoje'
+                    }
+                }
+            }
         }
     }
 });
 
-var menu2 = new xMenu.create({
-    id: '#meuMenu2',
-    itens: {
-        teste: {
-            html: 'abcd',
-            icon: 'fa-arrow-left'
-        },
-        teste2: {
-        }
-    }
-});
+// var menu2 = new xMenu.create({
+//     id: '#meuMenu2',
+//     itens: {
+//         teste: {
+//             html: 'abcd',
+//             icon: 'fa-arrow-left'
+//         },
+//         teste2: {
+//         }
+//     }
+// });
 
 window.menu1 = menu1;
+// window.menu2 = menu2;
+window.xMenu = xMenu;
--- xMenu2.js
+++ xMenu2.js
@@ -2,7 +2,10 @@ export default (function () {
 
     const versao = '2.0';
 
-    function create(param) {
+    function create(param, owner) {
+
+        if (this)
+            owner = this;
 
         var countItems = 0;
         var elContainerMenu = undefined; // essa variável serve para armazenar a refencia do container do menu após ter sido adicionado no body;
@@ -17,6 +20,7 @@ export default (function () {
             animate: undefined,
             onOpen: undefined,
             onCreate: undefined,
+            parent: undefined,
             callClick: {}
         };
 
@@ -33,6 +37,59 @@ export default (function () {
             background: '',
             elList: {},
 
+            createDivLeft(name, props) {
+                let divLeft = document.createElement('div');
+
+                let icon = document.createElement('i');
+                if (props.icon !== undefined)
+                    icon.classList = props.icon + ' icon';
+                divLeft.appendChild(icon);
+
+                if (props.checkbox && props.subMenu === undefined) {
+                    let label = document.createElement('label');
+                    label.classList.add('xMenuCheckLa');
+
+                    let inputCheck = document.createElement('input');
+                    inputCheck.setAttribute('type', 'checkbox');
+                    inputCheck.setAttribute('id', 'sw' + name + countItems);
+
+                    label.appendChild(inputCheck);
+                    divLeft.appendChild(label);
+                }
+
+                return divLeft;
+            },
+
+            createDivCenter(name, props) {
+                let divCenter = document.createElement('div');
+
+                let html = document.createElement('span');
+                html.innerHTML = props.html === undefined ? name : props.html;
+
+                divCenter.appendChild(html);
+                return divCenter;
+            },
+
+            createDivRight(name, props) {
+                let divRight = document.createElement('div');
+                let shortkey = document.createElement('span');
+
+                if (props.shortkey !== undefined && props.subMenu === undefined) {
+                    let [keyName, keyCode] = Object.entries(props.shortkey)[0];
+                    shortkey.classList.add('short');
+                    shortkey.innerHTML = keyName;
+                    keyDown[String(keyCode).toUpperCase()] = props.click;
+                }
+
+                if (props.subMenu !== undefined){
+                    shortkey.innerText = '►';
+                    shortkey.classList.add('short');
+                }
+
+                divRight.appendChild(shortkey);
+                return divRight;
+            },
+
             setMain(arg) {
                 this.element = document.createElement('ul');
                 this.element.setAttribute('id', 'xMenu_' + arg.id);
@@ -44,66 +101,38 @@ export default (function () {
 
             setList(arg) {
                 //loop que adiciona os itens passados para o menu
+
                 Object.entries(arg.itens).forEach(ln => {
                     let [name, props] = ln;
 
                     countItems++;
 
-                    let divCheckbox = document.createElement('div');
-                    if (props.checkbox) {
-                        let label = document.createElement('label');
-                        label.classList.add('xMenuCheckLa');
-
-                        let inputCheck = document.createElement('input');
-                        inputCheck.setAttribute('type', 'checkbox');
-                        inputCheck.setAttribute('id', 'sw' + arg.id + countItems);
-
-                        label.appendChild(inputCheck);
-                        divCheckbox.appendChild(label);
-                    }
-
-                    let divHtml = document.createElement('div');
-
-                    let icon = document.createElement('i');
-                    if (props.icon !== undefined)
-                        icon.classList = props.icon;
-
-                    let html = document.createElement('span');
-                    html.innerHTML = props.html === undefined ? name : props.html;
-
-                    divHtml.appendChild(icon);
-                    divHtml.appendChild(html);
-
-                    let divShortkey = document.createElement('div');
-                    if (props.shortkey !== undefined) {
-
-                        let shortkey = document.createElement('span');
-                        let [keyName, keyCode] = Object.entries(props.shortkey)[0];
-                        shortkey.classList.add('short');
-                        shortkey.innerHTML = keyName;
-                        keyDown[String(keyCode).toUpperCase()] = props.click;
-
-                        divShortkey.appendChild(shortkey);
-                    }
+                    let divLeft = this.createDivLeft(name, props);
+                    let divCenter = this.createDivCenter(name, props);
+                    let divRight = this.createDivRight(name, props);
 
                     let li = document.createElement('li');
                     li.setAttribute('id', 'xMenu_' + name);
 
-                    li.appendChild(divCheckbox);
-                    li.appendChild(divHtml);
+                    li.appendChild(divLeft);
+                    li.appendChild(divCenter);
+                    li.appendChild(divRight);
 
-                    if (divShortkey !== undefined) 
-                        li.appendChild(divShortkey);
-                    
                     if (props.click)
                         li.addEventListener('click', props.click);
 
+                    if (!props.subMenu) {
+                        li.addEventListener('click', () => {
+                            this.eventCloseClick(li);
+                        })
+                    }
+
                     if (props.checkbox) {
                         li.addEventListener('change', () => {
-                            if (divCheckbox.querySelector('label').classList.contains('checked'))
-                                divCheckbox.querySelector('label').classList.remove('checked')
+                            if (divLeft.querySelector('label').classList.contains('checked'))
+                                divLeft.querySelector('label').classList.remove('checked')
                             else
-                                divCheckbox.querySelector('label').classList.add('checked');
+                                divLeft.querySelector('label').classList.add('checked');
                         });
                     }
 
@@ -113,30 +142,42 @@ export default (function () {
                 });
             },
 
-            setBackground() {
+            setBackground(arg) {
+                if (arg.backgroundParent) {
+                    ax.background = arg.backgroundParent;
+                    return;
+                }
+
                 ax.background = document.createElement('div');
                 ax.background.setAttribute('id', 'pnBackgroudxMenu' + arg.id);
                 ax.background.classList.add('pnBackgroudxMenu');
                 ax.background.style.display = 'none';
             },
 
-            appendBody() {
-                document.body.append(this.element);
-                document.body.append(this.background);
+            append(arg) {
+                if (arg.parent) {
+                    arg.parent.appendChild(this.element);
+                }
+                else {
+                    document.body.append(this.element);
+                    document.body.append(this.background);
+                }
                 elContainerMenu = document.querySelector(arg.el)
             },
 
             setOpenMenu(arg) {
 
-                function openMenu(event) {
+                const openMenu = (event) => {
                     if (arg.onOpen)
                         arg.onOpen(ax.elList);
 
+                    let posParent = arg.parent ? arg.parent.getBoundingClientRect() : undefined;
+
                     var op = {
-                        top: event.pageY + 2,
-                        left: event.pageX + 2,
-                        heightMenu: parseInt(ax.element.height),
-                        widthMenu: parseInt(ax.element.style.width),
+                        top: posParent ? posParent.y : event.pageY + 2,
+                        left: posParent ? posParent.x + posParent.width : event.pageX + 2,
+                        heightMenu: parseFloat(33 * countItems),
+                        widthMenu: posParent ? posParent.width : 0,
                         innerHeight: window.innerHeight,
                         innerWidth: window.innerWidth
                     };
@@ -144,35 +185,55 @@ export default (function () {
                     if (op.innerHeight - (op.heightMenu + op.top) < 0)
                         op.top = op.top - op.heightMenu - 2;
 
-                    if (op.innerWidth - (op.widthMenu + op.left) < 0)
-                        op.left = op.left - op.widthMenu - 2;
+                    if (op.innerWidth - (op.left + op.widthMenu) < 0)
+                        op.left = op.left - op.widthMenu
 
                     ax.element.style.top = op.top + 'px';
                     ax.element.style.left = op.left + 'px';
                     ax.element.style.display = 'block';
                     ax.element.classList.remove('hide');
 
-
                     if (arg.animate !== undefined)
                         elContainerMenu.classList.add('animated ' + arg.animate);
 
-                    ax.background.style.display = 'block'
+                    if (ax.background)
+                        ax.background.style.display = 'block'
 
                     event.preventDefault();
                 }
 
-                let context = arg.buttonLeft === true ? 'click' : 'contextmenu';
-                elContainerMenu.addEventListener(context, openMenu)
+                const hideMenu = () => {
+                    ax.element.classList.add('hide');
+                }
+
+                if (arg.parent) {
+                    elContainerMenu.addEventListener('mouseover', openMenu)
+                    elContainerMenu.addEventListener('mouseout', hideMenu);
+                } else {
+                    let context = arg.buttonLeft === true ? 'click' : 'contextmenu';
+                    elContainerMenu.addEventListener(context, openMenu)
+                }
+
             },
 
             setCloseMenu() {
-                ax.background.addEventListener('click', this.eventCloseClick);
-                ax.element.addEventListener('click', this.eventCloseClick)
+                if (ax.background)
+                    ax.background.addEventListener('click', this.eventCloseClick);
             },
 
-            eventCloseClick() {
-                ax.background.style.display = 'none';
+            eventCloseClick(li = undefined) {
                 ax.element.classList.add('hide');
+                if (ax.background)
+                    ax.background.style.display = 'none'
+
+                if (li === undefined)
+                    return
+
+                let parentUL = li.parentNode.parentNode.parentNode;
+                if (parentUL.nodeName === 'UL') {
+                    parentUL.classList.add('hide');
+                    this.eventCloseClick(li.parentNode.parentNode);
+                }
             },
 
             disableAll(arg) {
@@ -286,33 +347,63 @@ export default (function () {
                     }
 
                 })
+            },
+
+            subMenuCreate(owner, arg) {
+                Object.entries(arg.itens).forEach(el => {
+                    let [name, props] = el;
+                    if (props.subMenu) {
+                        create({
+                            id: '#xMenu_' + name,
+                            itens: props.subMenu.itens,
+                            onOpen: props.subMenu.onOpen,
+                            parent: ax.elList[name],
+                            backgroundParent: ax.background
+                        }, owner);
+                    }
+                })
+            },
+
+            getDimensionsMenu() {
+                /* como a posição do menu é calculada antes do mesmo ser renderizado é
+                necessário clonar o objeto pra saber sua largura e altura e depois destrui-lo.*/
+                // let e = ax.element.lastChild;
+                // e.clodeNode(false);
+                // e.style.visibility = "hidden";
+                // document.body.appendChild(e);
+                // let height = e.offsetHeight + 0;
+                // let width = e.offsetWidth + 0;
+                // document.body.removeChild(e);
+                // e.style.visibility = "visible";
+                // return { height, width };
             }
 
         }
 
         ax.setMain(arg);
+        ax.setBackground(arg);
         ax.setList(arg);
-        ax.setBackground();
-        ax.appendBody();
+        ax.append(arg);
         ax.setOpenMenu(arg);
         ax.setCloseMenu(arg);
         ax.setDisable(arg);
         ax.setCreate(arg);
         ax.setkeyDown();
+        ax.subMenuCreate(owner, arg);
 
-        this.disableAll = () => ax.disableAll(arg);
+        owner.disableAll = () => ax.disableAll(arg);
 
-        this.enableAll = () => ax.enableAll(arg)
+        owner.enableAll = () => ax.enableAll(arg)
 
-        this.disableItem = (item) => ax.disableItem(item, arg);
+        owner.disableItem = (item) => ax.disableItem(item, arg);
 
-        this.enableItem = (item) => ax.enableItem(item, arg);
+        owner.enableItem = (item) => ax.enableItem(item, arg);
 
-        this.enable = (item, boolean) => ax.enable(item, boolean);
+        owner.enable = (item, boolean) => ax.enable(item, boolean);
 
-        this.setHtml = (item, html) => ax.setHtml(item, html, arg)
+        owner.setHtml = (item, html) => ax.setHtml(item, html, arg)
 
-        this.setIcon = (item, icon) => ax.setIcon(item, icon, arg);
+        owner.setIcon = (item, icon) => ax.setIcon(item, icon, arg);
 
     }
 
